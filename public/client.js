099c5d31b0704c397c1c18b669c677f9db3c75dc
Marija Jovanovic <marija.jo@hotmail.com>
2018-02-27 21:28:02 -0500

    add js

--- public/client.js
+++ public/client.js
@@ -3,3 +3,39 @@
 
 //Step 2
 //Using global variants, functions, objects (trigers)
+$('document').ready(function () {
+
+    $('#landing-page').show();
+    //    $('.images').show();
+    $('.login').hide();
+    $('.signup').hide();
+    $('#my-dashboard').hide();
+    $('.new-item').hide();
+
+
+
+    $("#login-button").click(function () {
+
+        $('.login').hide();
+        $('#landing-page').hide();
+        $('.images').hide();
+        $('.login').show();
+        $('.signup').hide();
+        $('#my-dashboard').hide();
+        $('.new-item').hide();
+
+    })
+
+    $("#register-button").click(function () {
+        $('.login').hide();
+        $('#landing-page').hide();
+        $('.images').hide();
+        $('.login').hide();
+        $('.signup').show();
+        $('#my-dashboard').hide();
+        $('.new-item').hide();
+
+
+    })
+
+});
